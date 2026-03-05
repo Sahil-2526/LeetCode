6714bb7af28e4656764ad8378a2aa6be91edb659
Sukehiro <sahil.26.02.2007@gmail.com>
2026-03-05 01:01:39 +0000

    problem 2114

---
+++ Easy/2114. Maximum Number of Words Found in Sentences.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int mostWordsFound(vector<string>& sentences) {
+        int len=sentences.size();
+        int result=0;
+        for(int i=0;i<len;i++){
+            int count=1;
+            int l=sentences[i].size();
+            for(int j=0;j<l;j++){
+                if(sentences[i][j]==' ') count++;
+            }
+            result=max(count,result);
+        }
+        return result;
+    }
+};
