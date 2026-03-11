1b72676dd985fa17b537bce0617ca9fe7558476a
Sukehiro <sahil.26.02.2007@gmail.com>
2026-03-11 10:24:38 +0000

    question 190 in cpp

--- Easy/190. Reverse Bits.py
+++ Easy/190. Reverse Bits.py
@@ -8,3 +8,19 @@ class Solution(object):
             product += 2 ** (31 - k) if n > 0 else 0
             n -= 2 ** k if n > 0 else 0
         return product
+
+
+# in cpp
+
+# class Solution {
+# public:
+#     int reverseBits(int n) {
+#         int mx= INT_MAX;
+#         int val=0;
+#         for(int i=0;i<32;i++){
+#             if(n%2!=0) val+=(int)pow(2,31-i)%mx;
+#             n=n>>1;
+#         }
+#         return val%mx;
+#     }
+# };
