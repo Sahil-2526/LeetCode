c38eefd9ca2adcd8ca8785dd3e4d4e588c465d10
Sukehiro <sahil.26.02.2007@gmail.com>
2026-03-21 03:29:28 +0000

    question 877

---
+++ Medium/877. Stone Game.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
+};
+
+// alice can choose even or odd indexed piles so she will always will win the game as she can always choose the maximum of the two options and bob will be left with the other option which is smaller than alice's choice
