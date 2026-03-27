443ebfa30bf5e7a68e0f64176d50ff0e06676c0a
Sukehiro <sahil.26.02.2007@gmail.com>
2026-03-27 10:01:41 +0000

    question 238

---
+++ Medium/238. Product of Array Except Self.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n= nums.size();
+
+        int total=1;
+        int zeroExist=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==0) zeroExist++;
+            else total*=nums[i];
+        }
+
+        vector<int> result(n,0);
+        for(int i=0;i<n;i++){
+            if(zeroExist>1) break;
+            if(zeroExist==1 && nums[i]==0) result[i]=total;
+            if(zeroExist==0) result[i]=total/nums[i];
+        }
+
+        return result;
+    }
+};
