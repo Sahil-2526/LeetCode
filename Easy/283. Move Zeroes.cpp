ea1f8805f932bd3f0fe70ee912b9c6d9c3182b9c
Sukehiro <sahil.26.02.2007@gmail.com>
2026-04-02 05:00:12 +0000

    question 283

---
+++ Easy/283. Move Zeroes.cpp
@@ -0,0 +1,18 @@
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int k=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                nums[k]=nums[i];
+                k++;
+            }
+        }
+
+        for(;k<nums.size();k++){
+            nums[k]=0;
+        }
+    }
+};
