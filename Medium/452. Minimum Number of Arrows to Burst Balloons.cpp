7d0cdaa1eaa9fc31a96c175f1efee0beade78b27
Sukehiro <sahil.26.02.2007@gmail.com>
2026-04-07 09:29:00 +0000

    question 452

---
+++ Medium/452. Minimum Number of Arrows to Burst Balloons.cpp
@@ -0,0 +1,25 @@
+#include<vector>
+#include<algorithm>
+using namespace std;
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& points) {
+        if(points.empty()) return 0;
+
+        sort(points.begin(), points.end(), [](vector<int> &a, vector<int> &b){
+            return a[1] < b[1];
+        });
+
+        int count = 1;             
+        int past = points[0][1];   
+
+        for(int i = 1; i < points.size(); i++){
+            if(points[i][0] > past){
+                count++;
+                past = points[i][1]; 
+            }
+        }
+
+        return count;
+    }
+};
