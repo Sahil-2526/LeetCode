4a5f090ed7a1f53301b84aece64758cec885742f
Sukehiro <sahil.26.02.2007@gmail.com>
2026-04-07 08:08:44 +0000

    question 986

---
+++ Medium/986. Interval List Intersections.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
+        int n = firstList.size();
+        int m = secondList.size();
+
+        vector<vector<int>> result;
+
+        int i = 0, j = 0;
+        while(i < n && j < m){
+            int l = max(firstList[i][0], secondList[j][0]);
+            int r = min(firstList[i][1], secondList[j][1]);
+
+            if(l <= r) result.push_back({l,r});
+
+            if(firstList[i][1] < secondList[j][1])
+                i++;
+            else
+                j++;
+        }
+
+        return result;
+    }
+};
