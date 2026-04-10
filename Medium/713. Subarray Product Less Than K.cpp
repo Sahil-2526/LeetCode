d756833e26a1e04e152aad0649e0c33dea2621b9
Sukehiro <sahil.26.02.2007@gmail.com>
2026-04-10 04:04:36 +0000

    question 713

---
+++ Medium/713. Subarray Product Less Than K.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        if (k <= 1) return 0;
+
+        int n = nums.size();
+        int l = 0;
+        long long product = 1;
+        int count = 0;
+
+        for (int r = 0; r < n; r++) {
+            product *= nums[r];
+
+            while (product >= k) {
+                product /= nums[l];
+                l++;
+            }
+
+            count += (r - l + 1);
+        }
+
+        return count;
+    }
+};
