83606b8c80f1b36cfc1b85d15d7f281acc54fe8c
Sukehiro <sahil.26.02.2007@gmail.com>
2026-04-15 10:17:06 +0000

    question 88

---
+++ Easy/88. Merge Sorted Array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int l = m + n - 1; // last position
+        m--; 
+        n--;
+
+        while (m >= 0 && n >= 0) {
+            if (nums1[m] > nums2[n]) {
+                nums1[l--] = nums1[m--];
+            } else {
+                nums1[l--] = nums2[n--];
+            }
+        }
+
+        while (n >= 0) {
+            nums1[l--] = nums2[n--];
+        }
+    }
+};
