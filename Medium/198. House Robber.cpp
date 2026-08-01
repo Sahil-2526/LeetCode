f96978d60d52ac3fd3d07917d941eafcd6801c23
Sukehiro <sahil.26.02.2007@gmail.com>
2026-08-01 09:47:31 +0000

    question 198

---
+++ Medium/198. House Robber.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if( n == 1) return nums[0];
+        vector<int> dp(n+1,0);
+        dp[0] = 0;
+        dp[1] = nums[0];
+        dp[2] = nums[1];
+        for(int i=3; i<=n; i++){
+            int val = max(dp[i-2],dp[i-3]);
+            dp[i] = val+nums[i-1];
+        }
+        return max(dp[n],dp[n-1]);
+    }
+};
