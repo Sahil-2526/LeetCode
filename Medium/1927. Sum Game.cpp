2a3db4662c9053715606870b23d01c232f0a3e8f
Sukehiro <sahil.26.02.2007@gmail.com>
2026-08-23 01:11:29 +0000

    question 1927

---
+++ Medium/1927. Sum Game.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool sumGame(string num) {
+        int n = num.size();
+        int l = 0, r = 0;
+        int ls = 0, rs = 0;
+
+        for (int i = 0; i < n / 2; i++) {
+            if (num[i] == '?') l++;
+            else ls += num[i] - '0';
+        }
+
+        for (int i = n / 2; i < n; i++) {
+            if (num[i] == '?') r++;
+            else rs += num[i] - '0';
+        }
+
+        int q = l-r;
+        int s = ls-rs;
+
+        return q % 2 != 0 || s != -9*q/2 ;
+    }
+};
