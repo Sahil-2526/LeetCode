7ae9c5493fe0f231106763e0cc3365bb061a9b5c
Sukehiro <sahil.26.02.2007@gmail.com>
2026-09-08 01:18:59 +0000

    question 3870

---
+++ Easy/3870. Count Commas in Range.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countCommas(int n) {
+        int ans = 0;
+        int p = 1000;
+
+        while (p <= n) {
+            ans += n-p+1;
+            p *= 1000;
+        }
+
+        return (int)ans;
+    }
+};
